1e0758c4600a2fe6442fbb8593cc0a058c12d035
Sem1ra1 <theonly.lightfall002@gmail.com>
2026-09-21 21:33:58 +0300

    Fresh lazy.nvim with colorscheme only

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,11 +1,6 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-vim.g.snacks_animate = false
 vim.g.autoformat = false
 vim.opt.guicursor = "n-v-c-i-sm:block,r-cr-o:hor20"
 vim.opt.iskeyword:append("-")
-vim.opt.scrolloff = 999  -- or any number you like
+vim.opt.scrolloff = 999
 vim.opt.swapfile = false
 vim.opt.clipboard = "unnamedplus"
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,10 @@
--- return {
--- 	"olimorris/onedarkpro.nvim",
--- 	priority = 1000, -- Ensure it loads first
--- 	lazy = false,
--- }
 return {
-  "idr4n/andromeda.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme andromeda")
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 }
