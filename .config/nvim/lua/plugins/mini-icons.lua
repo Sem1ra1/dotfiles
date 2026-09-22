06c73dc58e24d3a1c92de4ebbd0d77cbe7ae46f7
Sem1ra1 <theonly.lightfall002@gmail.com>
2026-09-22 18:29:35 +0300

    added mini-icons plugin for icons throughout all neovim ui
    Icons for tabs don't work, apparently

---
+++ .config/nvim/lua/plugins/mini-icons.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "nvim-mini/mini.icons",
+    opts = {
+      style = "glyph",
+
+-- Setting up some missing icons
+      file = {
+        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+        [".gitconfig"] = { glyph = "󰊢", hl = "MiniIconsPurple" },
+        [".stow-local-ignore"] = { glyph = "󰌷", hl = "MiniIconsGrey" },
+        ["nvim"] = { glyph = "󰉋", hl = "MiniIconsAzure" },
+        [".config"] = { glyph = "󰉋", hl = "MiniIconsAzure" },
+        ["plugins"] = { glyph = "󰉋", hl = "MiniIconsAzure" },
+        [".initial_startup_done"] = { glyph = "󰒓", hl = "MiniIconsGrey" },
+        ["config"] = { glyph = "󰉋", hl = "MiniIconsAzure" },
+        ["UserModules"] = { glyph = "󰉋", hl = "MiniIconsAzure" },
+      },
+
+      filetype = {
+        dotenv = { glyph = "", hl = "MiniIconsYellow" },
+        stow = { glyph = "󰌷", hl = "MiniIconsBlue" },
+        node_modules = { glyph = "", hl = "MiniIconsRed" },
+
+      },
+
+      directory = {},
+      default = {
+        file = { glyph = "󰈔", hl = "MiniIconsGrey", },
+        directory = { glyph = "󰉋", hl = "MiniIconsAzure" },
+      },
+      extension = {
+        ["list"] = { glyph = "", hl = "MiniIconsGrey" },
+        ["sh"] = { glyph = "", hl = "MiniIconsGrey" },
+        ["text"] = { glyph = "", hl = "MiniIconsGrey" },
+      },
+      lsp       = {},
+      os        = {},
+    },
+  },
+}
