025346d91ba4de7d400b2f1e804fb78db364d4a4
Sem1ra1 <theonly.lightfall002@gmail.com>
2026-09-24 00:40:27 +0300

    changed hyprlock fonts

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,96 +1,106 @@
+-- return {
+-- 	"idr4n/andromeda.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+--
+-- 	config = function()
+--
+-- 		require("andromeda").setup({
+-- 			transparent = false, -- Disable background color
+-- 			terminal_colors = true, -- Configure terminal colors
+--
+-- 			styles = {
+-- 				comments = { italic = true },
+-- 				keywords = { italic = true },
+-- 				functions = {},
+-- 				variables = {},
+-- 				floats = "normal", -- "dark" | "transparent" | "normal"
+-- 				sidebars = "normal", -- "dark" | "transparent" | "normal"
+-- 			},
+--
+-- 			plugins = {
+-- 				all = true, -- Enable all plugin integrations
+-- 				-- Selectively disable plugins:
+-- 				-- telescope = false,
+-- 				-- neo_tree = false,
+-- 			},
+--
+-- 			-- Override specific colors
+-- 			on_colors = function(colors)
+-- 				-- Base color palette
+-- 				colors.cyan = "#44d2d4" -- Primary accent, identifiers
+-- 				colors.pink = "#ff00aa" -- Secondary accent, properties
+-- 				colors.purple = "#c74ded" -- Keywords, types
+-- 				colors.orange = "#f39c12" -- Numbers
+-- 				colors.yellow = "#ffe66d" -- Functions
+-- 				colors.magenta = "#f92672" -- Tags, special
+-- 				colors.blue = "#7cb7ff" -- Strings (regex)
+-- 				colors.red = "#ee5d43" -- Errors, booleans
+-- 				colors.green = "#96e072" -- Strings
+--
+-- 				-- Background scale
+-- 				colors.bg = "#23262e"
+-- 				colors.bg_highlight = "#2f323c"
+-- 				colors.bg_visual = "#3a3e4b"
+-- 				colors.bg_search = "#464959"
+--
+-- 				-- Foreground scale
+-- 				colors.fg_dark = "#606064"
+-- 				colors.fg_gutter = "#81898F"
+-- 				colors.fg = "#d5ced9"
+--
+-- 				-- Semantic colors
+-- 				colors.error = "#ee5d43"
+-- 				colors.warning = "#ff7700"
+-- 				colors.info = "#47eae0"
+-- 				colors.hint = "#12877F"
+--
+-- 				-- Git colors
+-- 				colors.git_add = "#00ff77"
+-- 				colors.git_change = "#47eae0"
+-- 				colors.git_delete = "#ff0038"
+-- 			end,
+--
+-- 			-- Override specific highlights
+-- 			on_highlights = function(highlights, colors)
+-- 				highlights.Comment = { italic = true }
+-- 				highlights.Constant = { fg = colors.red }
+-- 				highlights.Title = { fg = colors.foreground } -- html text inside 'title' tag
+-- 				highlights.cssTagName = { fg = colors.magenta }
+-- 				highlights.cssCascadeProp = { fg = colors.red }
+-- 				highlights["@punctuation.special"] = { fg = colors.yellow } -- curly brackets for 'string interpolation'
+-- 				highlights["@punctuation.bracket"] = { fg = colors.yellow } -- curly brackets for functions
+-- 				highlights["@constant.html"] = { fg = colors.magenta, italic = true } -- root 'DOCTYPE' tag
+-- 				highlights["@keyword.return"] = { fg = colors.magenta, italic = true } -- root 'DOCTYPE' tag
+-- 				highlights["@markup.link.label.html"] = { fg = colors.foreground } -- root 'DOCTYPE' tag
+-- 				highlights["@string.css"] = { fg = colors.orange } -- root 'DOCTYPE' tag
+-- 				highlights["@operator.css"] = { fg = colors.magenta } -- root 'DOCTYPE' tag
+-- 				highlights["@operator.css"] = { fg = colors.magenta } -- root 'DOCTYPE' tag
+-- 				highlights["@function.css"] = { fg = colors.orange } -- root 'DOCTYPE' tag
+-- 				-- highlights["@property.css"] = { fg = colors.fg } -- root 'DOCTYPE' tag
+-- 				highlights["@punctuation.bracket.css"] = { fg = colors.yellow } -- root 'DOCTYPE' tag
+-- 				highlights["@variable.builtin.javascript"] = { fg = colors.purple } -- root 'DOCTYPE' tag
+-- 				-- highlights["@operator.javascript"] = { fg = colors.magenta } -- root 'DOCTYPE' tag
+-- 				highlights.SnacksPickerPathHidden = { fg = colors.fg_gutter }
+-- 				highlights.SnacksPickerPathIgnored = { fg = colors.fg_dark }
+-- 				highlights.Constant = { fg = colors.red }
+-- 				highlights.Title = { fg = colors.foreground }
+-- 				highlights.CursorLine = { bg = colors.bg_highlight }
+-- 				highlights.BlinkCmpKind = { bg = colors.bg }
+--
+-- 			end,
+-- 		})
+--
+-- 		vim.cmd("colorscheme andromeda")
+-- 	end,
+-- }
+--
 return {
-	"idr4n/andromeda.nvim",
-	lazy = false,
-	priority = 1000,
-
-	config = function()
-
-		require("andromeda").setup({
-			transparent = false, -- Disable background color
-			terminal_colors = true, -- Configure terminal colors
-
-			styles = {
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = {},
-				variables = {},
-				floats = "normal", -- "dark" | "transparent" | "normal"
-				sidebars = "normal", -- "dark" | "transparent" | "normal"
-			},
-
-			plugins = {
-				all = true, -- Enable all plugin integrations
-				-- Selectively disable plugins:
-				-- telescope = false,
-				-- neo_tree = false,
-			},
-
-			-- Override specific colors
-			on_colors = function(colors)
-				-- Base color palette
-				colors.cyan = "#44d2d4" -- Primary accent, identifiers
-				colors.pink = "#ff00aa" -- Secondary accent, properties
-				colors.purple = "#c74ded" -- Keywords, types
-				colors.orange = "#f39c12" -- Numbers
-				colors.yellow = "#ffe66d" -- Functions
-				colors.magenta = "#f92672" -- Tags, special
-				colors.blue = "#7cb7ff" -- Strings (regex)
-				colors.red = "#ee5d43" -- Errors, booleans
-				colors.green = "#96e072" -- Strings
-
-				-- Background scale
-				colors.bg = "#23262e"
-				colors.bg_highlight = "#2f323c"
-				colors.bg_visual = "#3a3e4b"
-				colors.bg_search = "#464959"
-
-				-- Foreground scale
-				colors.fg_dark = "#606064"
-				colors.fg_gutter = "#81898F"
-				colors.fg = "#d5ced9"
-
-				-- Semantic colors
-				colors.error = "#ee5d43"
-				colors.warning = "#ff7700"
-				colors.info = "#47eae0"
-				colors.hint = "#12877F"
-
-				-- Git colors
-				colors.git_add = "#00ff77"
-				colors.git_change = "#47eae0"
-				colors.git_delete = "#ff0038"
-			end,
-
-			-- Override specific highlights
-			on_highlights = function(highlights, colors)
-				highlights.Comment = { italic = true }
-				highlights.Constant = { fg = colors.red }
-				highlights.Title = { fg = colors.foreground } -- html text inside 'title' tag
-				highlights.cssTagName = { fg = colors.magenta }
-				highlights.cssCascadeProp = { fg = colors.red }
-				highlights["@punctuation.special"] = { fg = colors.yellow } -- curly brackets for 'string interpolation'
-				highlights["@punctuation.bracket"] = { fg = colors.yellow } -- curly brackets for functions
-				highlights["@constant.html"] = { fg = colors.magenta, italic = true } -- root 'DOCTYPE' tag
-				highlights["@keyword.return"] = { fg = colors.magenta, italic = true } -- root 'DOCTYPE' tag
-				highlights["@markup.link.label.html"] = { fg = colors.foreground } -- root 'DOCTYPE' tag
-				highlights["@string.css"] = { fg = colors.orange } -- root 'DOCTYPE' tag
-				highlights["@operator.css"] = { fg = colors.magenta } -- root 'DOCTYPE' tag
-				highlights["@operator.css"] = { fg = colors.magenta } -- root 'DOCTYPE' tag
-				highlights["@function.css"] = { fg = colors.orange } -- root 'DOCTYPE' tag
-				-- highlights["@property.css"] = { fg = colors.fg } -- root 'DOCTYPE' tag
-				highlights["@punctuation.bracket.css"] = { fg = colors.yellow } -- root 'DOCTYPE' tag
-				highlights["@variable.builtin.javascript"] = { fg = colors.purple } -- root 'DOCTYPE' tag
-				-- highlights["@operator.javascript"] = { fg = colors.magenta } -- root 'DOCTYPE' tag
-				highlights.SnacksPickerPathHidden = { fg = colors.fg_gutter }
-				highlights.SnacksPickerPathIgnored = { fg = colors.fg_dark }
-				highlights.Constant = { fg = colors.red }
-				highlights.Title = { fg = colors.foreground }
-				highlights.CursorLine = { bg = colors.bg_highlight }
-				highlights.BlinkCmpKind = { bg = colors.bg }
-
-			end,
-		})
-
-		vim.cmd("colorscheme andromeda")
-	end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function() 
+    vim.cmd[[colorscheme tokyonight-night]]
+  end
 }
