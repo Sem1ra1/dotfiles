5b93b9e08e5a265efd84b320bab3e923bbb4d605
Sem1ra1 <theonly.lightfall002@gmail.com>
2026-09-23 11:59:54 +0300

    added colored cursors for each vim mode

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,3 +9,27 @@ vim.opt.iskeyword:append("-")
 vim.opt.scrolloff = 999  -- or any number you like
 vim.opt.swapfile = false
 vim.opt.clipboard = "unnamedplus"
+vim.opt.guicursor = {
+  "n-v-c:block-Cursor",
+  "i:block-Cursor",
+  "r:block-Cursor",
+}
+
+local cursor_colors = {
+  n = "#dddddd",
+  i = "#00e8c6",
+  v = "#cba6f7",
+  V = "#cba6f7",
+  R = "#f38ba8",
+}
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  callback = function()
+    local color = cursor_colors[vim.fn.mode()] or "#ffcc66"
+
+    vim.api.nvim_set_hl(0, "Cursor", {
+      bg = color,
+      fg = "#212733",
+    })
+  end,
+})
